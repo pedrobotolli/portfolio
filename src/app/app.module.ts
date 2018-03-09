498c320b5503ee6478ba4d5bda0dc848829d60d1
Pedro Botolli <pedrobotolli.santos@gmail.com>
2018-03-09 10:37:17 -0300

    alterações na url base e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { ProjetosComponent } from './projetos/projetos.component';
 
 
 const APP_ROUTES: Routes = [
-  { path: '/portfolio/projetos', component: ProjetosComponent },
-  { path: '/portfolio/', component: HomeComponent}
+  { path: 'portfolio/projetos', component: ProjetosComponent },
+  { path: 'portfolio/', component: HomeComponent}
 ];
 
 @NgModule({
